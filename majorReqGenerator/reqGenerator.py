593af6c98f74d10ed6f94f69489a472e05dbc65d
Shaoyi Zhang <zhangshaoyi1995@gmail.com>
2016-09-10 00:51:02 -0700

    move abandoned code to temp.py

--- majorReqGenerator/reqGenerator.py
+++ majorReqGenerator/reqGenerator.py
@@ -4,46 +4,10 @@ import json
 import sys
 import re
 from collections import OrderedDict
-db = SqliteDatabase('course.db')
 
-def string2Json(ifile):
-    jsonStr = open(ifile,'rt').read()
-    jsonList = json.loads(jsonStr)
-    return jsonList
 
-# class Course(Model):
-#     Department = CharField()
-#     Number = DateField()
-#     JsonID = IntegerField()
 
-#     #is_relative = BooleanField()
 
-#     class Meta:
-#         database = db # This model uses the "people.db" database.
-
-# db.connect()
-
-# try:
-#     db.create_tables([Course])
-# except:
-#     pass
-
-courseList = string2Json('../courseData.json')
-
-# try:
-#     for course in courseList:
-#         newCourse = Course(Department = course['sub'], Number = course['number'], JsonID = course['id'])
-#         newCourse.save()
-# except:
-#     pass
-
-    # course['majorlimit']
-    # course['levellimit']
-
-    # course['semester']
-    # course['gearea']
-    # course['units']
-    # course['label']
 
 def mapDept2Abbrev():
     with open('deptTrans.txt') as f:
@@ -61,11 +25,7 @@ courseNumPattern1 = re.compile(r'\b[0-9]*[0-9A-Z]\b')
 # 13AH-BH-CH
 courseNumPattern2 = re.compile(r'\b[0-9]+?[A-Z]*\b')
 
-
-
 deptPattern = re.compile(r'\b([A-Z][a-z]+)\s?[0-9]+')
-
-
 
 multiEmptyLinePattern = re.compile(r'\n[\n|\s]+')
 
@@ -74,22 +34,7 @@ text = parser.from_file('pdf/Phys/Physics-BS_2016.pdf')
 # text = parser.from_file('pdf/PStat/Statistics-BS-2016.pdf')
 
 
-# convert 13AH-BH-CH in to ['13AH', '13BH', '13CH']
-# numPattern = re.compile(r'([0-9]+)[A-Za-z]+')
-# def regainCourseNum(courseList,index=1):
-#     lst = courseList
-#     indx = index
-#     for j in range(index,len(lst)):
-#         # print('aaa')
-#         for i in range(index-1,-1,-1):
-#             try:
-#                 num = re.match(numPattern,lst[i]).group()
-#                 lst[j] = num + lst[j]
-#                 break
-#                 print(num)
-#             except:
-#                 continue
-#     return lst
+
 numPattern2 = re.compile(r'([0-9]+)[A-Z]?')
 def regainCourseNum(courseList):
     if courseList == []:
@@ -107,37 +52,6 @@ def regainCourseNum(courseList):
             outList.append(courseList[i])
         # print(outList)
     return outList
-
-
-# a = ['13AH','BH','CH']
-# print(regainCourseNum(a))
-# def createDeptList(deptSet):
-#     deptList = list(deptSet)
-#     keys = NAME_MAP.keys()
-#     outList = []
-#     for dept in deptList:
-#         try:
-#             outList.append(NAME_MAP[dept])
-#         except:
-#             for key in keys:
-#                 if dept in key:
-#                     outList.append(NAME_MAP[key])
-#     print(outList)
-
-# def guessDept(deptSet, numList):
-#     deptList = list(deptSet)
-#     deptIndex = 0
-#     for dept in deptList:
-
-# def checkDept(dept,numList):
-#     for course in Course.select().where(Course.Department == dept):
-#         for num in numList:
-#             if course.Number != num:
-#                 return False
-#     return True
-            
-#             #print("{} {}".format(course.Department,course.Number),end='           ')
-
 
 
 lst = ['Mathematics','Chemistry','Physics']
